9d9ee9583155ba5dcbb188b13cb6ef2214126abd
Toby Donaldson <tjd@sfu.ca>
2022-06-18 22:12:04 -0700

    updated

--- docs/languages/racket/primes.rkt
+++ docs/languages/racket/primes.rkt
@@ -5,43 +5,40 @@
 ;;
 ;; Returns a list of all the divisors of n.
 ;;
-;; Works be removing from the list (1 2 ... n) all the
-;; numbers that have a non-zero remainder when divided
-;; into n.
+;; Works by keeping all the numbers on the list
+;; (1 2 ... (sqrt n)) that divide evenly into n.
 ;;
-;; This is not the most efficient way to generate the
-;; the divisors of n. Faster would be to check the numbers
-;; up to, and including, the square root of n.
-;;
-(define (divisors n)
+(define (half-divisors n)
   (filter (lambda (d) (= 0 (remainder n d)))
-          (range 1 (+ n 1))))
- 
+          (range 1 (+ 1 (round (sqrt n))))))
 
 ;;
 ;; Returns true when n is prime, and false otherwise.
 ;;
-;;
 ;; Not as efficent as it could be. You don't need all
-;; divisors of n, you just need to know that the smallest
-;; one is between 1 and n.
+;; divisors of n, just the smallest one between 1 and n.
 ;;
 (define (is-prime1? n)
   (cond [(< n 2) #f]
         [(= n 2) #t]
         [(= 0 (remainder n 2)) #f]
-        [else (= 2 (length (divisors n)))]))
+        [else (= 1 (length (half-divisors n)))]))
 
 ;;
 ;; Returns true when n is prime, and false otherwise.
 ;;
-;; Simpler implementation that the previous version, but not as
+;; Simpler implementation than the previous version, but not as
 ;; efficient.
 ;;
 (define (is-prime2? n)
-  (= 2 (length (divisors n))))
+  (= 1 (length (half-divisors n))))
 
-(define (primes-less-than n)
-  (length (filter is-prime2? (range n))))
-;; > (primes-less-than 10000)
+;;
+;; Returns the number of primes less than n.
+;;
+(define (num-primes-less-than n)
+  (length (filter is-prime1? (range 2 n))))
+;;
+;; > (time (num-primes-less-than 10000))
+;; cpu time: 47 real time: 40 gc time: 31
 ;; 1229
